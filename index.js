dd7ee10c3924f5b6b269c40eaf4eecf30a9641f5
imranKpathan <khanmech26@gmail.com>
2019-09-28 00:06:38 +0100

    Code Fix in index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.set('visits', 0);
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
         res.send('Number of Visits To The App:: ' + visits);
-        client.set('visits', parent(visits) + 1);
+        client.set('visits', parseInt(visits) + 1);
     });
 });
 
